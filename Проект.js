33b7508cb32639de95c3fa5f6ded6a0dfb392ff1
mrWafffffleh <waffleh@mail.ru>
2025-04-30 19:56:56 +0500

    Изменения

--- Проект.js
+++ Проект.js
@@ -1,10 +1,7 @@
 let lengthRail = document.getElementById("length")
 let defects = document.getElementById("defects")
 let def = []
-
-def = defects.value.split(",")
-def.sort()
-
+let borders = []
 function scanner(){
     if(lengthRail.value == ""){
         alert("Введите длину рельсы")
@@ -14,11 +11,33 @@ function scanner(){
         alert("Введите координаты дефектов")
         return;
     }
+    def = defects.value.split(",")
+    def.sort()
     let checkpoints = []
     let foundDefects = new Set()
     let LaserCord = 0
-    while(LaserCord <= lengthRail.value){
+    while(LaserCord <= lengthRail.value) {
+        if (def.includes(LaserCord)) {
+            foundDefects.add(LaserCord)
+        }
 
     }
-
+}
+function checkColor(LaserCord, isRight){
+    let minDistance = 11
+    for (const laserCordElement of def) {
+        let distance = laserCordElement - LaserCord
+        if(distance < minDistance){
+            minDistance = distance
+        }
+    }
+    if(Math.abs(minDistance) <= 5){
+        return "red"
+    }
+    else if(Math.abs(minDistance) <= 10 && Math.abs(minDistance) > 5){
+        return "yellow"
+    }
+    else{
+        return "green"
+    }
 }
